1a976dfe4261a9e0dd20ed0f29a0d7b454786866
Gary Norman <gary.norman.th@gmail.com>
2023-08-05 23:01:35 +0700

    improved fullscreen buttons

--- src/index.js
+++ src/index.js
@@ -305,3 +305,41 @@ function closeFullscreen() {
     document.msExitFullscreen();
   }
 }
+
+// if(document.fullscreenEnabled || document.webkitFullscreenEnabled) {
+// 	const toggleBtn = document.querySelector('.js-toggle-fullscreen-btn');
+
+// 	const styleEl = document.createElement('link');
+// 	styleEl.setAttribute('rel', 'stylesheet');
+// 	styleEl.setAttribute('href', 'https://codepen.io/tiggr/pen/poJoLyW.css');
+// 	styleEl.addEventListener('load', function() {
+// 		toggleBtn.hidden = false;
+// 	});
+// 	document.head.appendChild(styleEl);
+	
+// 	toggleBtn.addEventListener('click', function() {
+// 		if(document.fullscreen) {
+// 			document.exitFullscreen();
+// 		} else if(document.webkitFullscreenElement) {
+// 			document.webkitCancelFullScreen()
+// 		} else if(document.documentElement.requestFullscreen) {
+// 			document.documentElement.requestFullscreen();
+// 		} else {
+// 			document.documentElement.webkitRequestFullScreen();
+// 		}
+// 	});
+	
+// 	document.addEventListener('fullscreenchange', handleFullscreen);
+// 	document.addEventListener('webkitfullscreenchange', handleFullscreen);
+	
+	
+// 	function handleFullscreen() {
+// 		if(document.fullscreen || document.webkitFullscreenElement) {
+// 			toggleBtn.classList.add('on');
+// 			toggleBtn.setAttribute('aria-label', 'Exit fullscreen mode');
+// 		} else {
+// 			toggleBtn.classList.remove('on');
+// 			toggleBtn.setAttribute('aria-label', 'Enter fullscreen mode');
+// 		}
+// 	}
+// }
